0d131c66850e895f5f48a19b20e1a1d131eead6c
David Hafen <dhafen21@gmail.com>
2021-11-16 06:44:45 -0600

    Removed run_live in wrong spot

--- Run.py
+++ Run.py
@@ -4,7 +4,6 @@ from VolatilityTradingStrategy import *
 
 if __name__== "__main__":
     api = tradeapi.REST(paper_api_key_hafen_school, paper_secret_key_hafen_school, paper_url, api_version='v2')
-    run_live()
     #
     # while not api.get_clock().is_open:
     #     time.sleep(300)
